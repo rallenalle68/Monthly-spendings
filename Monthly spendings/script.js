7ab0f3486e07c8b081c1c5df550319b723dbb725
Rasmus Seppänen <rasmussep@gmail.com>
2020-06-30 16:59:25 +0200

    First commit: bad naming!

---
+++ Monthly spendings/script.js
@@ -0,0 +1,26 @@
+
+const addThings = () => {
+    let modal = document.getElementById('addThings-modal');
+    modal.style.top = '100px'
+
+    let TheModalBackground = document.getElementById('add-modal');
+
+    TheModalBackground.style.width = '100%'
+    TheModalBackground.style.height = '100%'
+    TheModalBackground.style.top = '0'
+    TheModalBackground.style.left = '0'
+    TheModalBackground.style.backgroundColor = 'rgba(0, 0, 0, 1)'
+}
+
+const closeAddThings = () => {
+    let modal = document.getElementById('addThings-modal');
+    modal.style.top = '-700px'
+
+    let TheModalBackground = document.getElementById('add-modal');
+
+    TheModalBackground.style.width = '0'
+    TheModalBackground.style.height = '0%'
+    TheModalBackground.style.top = '50%'
+    TheModalBackground.style.left = '50%'
+    TheModalBackground.style.backgroundColor = 'rgba(0, 0, 0, 0.0)'
+} 
