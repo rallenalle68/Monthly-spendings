b3fb9e5aa9ffd8e8d1d27d50da1630b24374366c
Rasmus Seppänen <rasmussep@gmail.com>
2020-06-30 21:14:44 +0200

    starting the javaScript now

--- Monthly spendings/script.js
+++ Monthly spendings/script.js
@@ -1,26 +1,25 @@
-
 const addThings = () => {
-    let modal = document.getElementById('addThings-modal');
-    modal.style.top = '100px'
+   let modal = document.getElementById("addThings-modal");
+   modal.style.top = "100px";
 
-    let TheModalBackground = document.getElementById('add-modal');
+   let TheModalBackground = document.getElementById("add-modal");
 
-    TheModalBackground.style.width = '100%'
-    TheModalBackground.style.height = '100%'
-    TheModalBackground.style.top = '0'
-    TheModalBackground.style.left = '0'
-    TheModalBackground.style.backgroundColor = 'rgba(0, 0, 0, 1)'
-}
+   TheModalBackground.style.width = "100%";
+   TheModalBackground.style.height = "100%";
+   TheModalBackground.style.top = "0";
+   TheModalBackground.style.left = "0";
+   TheModalBackground.style.backgroundColor = "rgba(0, 0, 0, 1)";
+};
 
 const closeAddThings = () => {
-    let modal = document.getElementById('addThings-modal');
-    modal.style.top = '-700px'
+   let modal = document.getElementById("addThings-modal");
+   modal.style.top = "-1000px";
 
-    let TheModalBackground = document.getElementById('add-modal');
+   let TheModalBackground = document.getElementById("add-modal");
 
-    TheModalBackground.style.width = '0'
-    TheModalBackground.style.height = '0%'
-    TheModalBackground.style.top = '50%'
-    TheModalBackground.style.left = '50%'
-    TheModalBackground.style.backgroundColor = 'rgba(0, 0, 0, 0.0)'
-} 
+   TheModalBackground.style.width = "0";
+   TheModalBackground.style.height = "0%";
+   TheModalBackground.style.top = "50%";
+   TheModalBackground.style.left = "50%";
+   TheModalBackground.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
+};
