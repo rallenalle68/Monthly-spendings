455fa312d61b995d7087c4dbc1b66585a25bedcd
Rasmus Seppänen <rasmussep@gmail.com>
2020-07-03 15:04:11 +0200

    trying to crete elements

--- Monthly spendings/script.js
+++ Monthly spendings/script.js
@@ -23,3 +23,53 @@ const closeAddThings = () => {
    TheModalBackground.style.left = "50%";
    TheModalBackground.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
 };
+
+const saveMoney = () => {
+   closeAddThings();
+
+   let spendingsDate = document.getElementById("date-input").value;
+   let spendingsCategorie = document.getElementById("categorie-input").value;
+   let spendingsAmount = document.getElementById("amount-input").value;
+
+   let earningsDate = document.getElementById("Edate-input").value;
+   let earningsCategorie = document.getElementById("Ecategorie-input").value;
+   let earningsAmount = document.getElementById("Eamount-input").value;
+
+   const container = document.getElementById("container");
+
+   let addedthing = document.createElement("div");
+   addedthing.classList = "addedThing";
+
+   let hello = document.createElement("div");
+   hello.classList = "hello";
+   hello.innerHTML = spendingsDate;
+
+   let hello1 = document.createElement("div");
+   hello1.classList = "hello1";
+   hello1.innerHTML = spendingsAmount;
+
+   let hello2 = document.createElement("div");
+   hello2.classList = "hello2";
+   hello2.innerHTML = spendingsCategorie;
+
+   let hello3 = document.createElement("div");
+   hello3.classList = "hello3";
+   hello3.innerHTML = earningsDate;
+
+   let hello4 = document.createElement("div");
+   hello4.classList = "hello4";
+   hello4.innerHTML = earningsAmount;
+
+   let hello5 = document.createElement("div");
+   hello5.classList = "hello5";
+   hello5.innerHTML = earningsCategorie;
+
+   container.appendChild(addedthing);
+
+   addedthing.appendChild(hello);
+   addedthing.appendChild(hello1);
+   addedthing.appendChild(hello2);
+   addedthing.appendChild(hello3);
+   addedthing.appendChild(hello4);
+   addedthing.appendChild(hello5);
+};
